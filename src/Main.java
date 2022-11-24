efadfb4fdeca4203fecd5a583aef0d4e3d218b11
milas <supeeva@yandex.ru>
2022-11-25 03:00:49 +0400

    ДЗ Задачи с 4 по 8

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,53 @@ public class Main {
         System.out.println("За 3 суток машина произвела бутылок " + day * 3 + " штук");
         System.out.println("За месяц машина произвела бутылок " + day * 30 + " штук");
 
+        // Задача 5
+        System.out.println("Задача 5");
+        int totalJars = 120;
+        int whiteJarsForOneClass = 2;
+        int brounJarsForOneClass = 4;
+        int totalClasses = totalJars / (whiteJarsForOneClass + brounJarsForOneClass);
+        int totalWhiteJars = totalClasses * whiteJarsForOneClass;
+        int totalBrounJars = totalClasses * brounJarsForOneClass;
+        System.out.println("В школе, где " + totalClasses + " классов, нужно " + totalWhiteJars + " банок белой краски и " + totalBrounJars + " банок коричневой краски");
+
+        // Задача 6
+        System.out.println("Задача 6");
+        int bananasWeight = 5 * 80;
+        int milkWeight = 105 * 2;
+        int icecreamWeight = 100 * 2;
+        int eggsWeight = 4* 70;
+        int breakfastWeightInGramm = bananasWeight + milkWeight + icecreamWeight + eggsWeight;
+        float breakfastWeightInKilogramm = breakfastWeightInGramm / (float) 1000;
+        System.out.println(breakfastWeightInGramm);
+
+        // Задача 7
+        System.out.println("Задача 7");
+        int loseWeight = 7000;
+        int firstOption = 250;
+        int secondOption = 500;
+        int totalFirstOption = loseWeight / firstOption;
+        int totalSecondOption = loseWeight / secondOption;
+        System.out.println("Если спортсмен будет терять по " + firstOption + " грамм в день, то ему понадобится " + totalFirstOption + " дней");
+        System.out.println("Если спортсмен будет терять по " + secondOption + " грамм в день, то ему понадобится " + totalSecondOption + " дней");
+        System.out.println("В среднем для похудения спортсмену понадобится " + (totalFirstOption + totalSecondOption) / 2 + " день" );
+
+        // Задача 8
+        System.out.println(" Задача 8 ");
+        var masha = 67760;
+        var denis = 83690;
+        var kristina = 76230;
+        var mashaNewSalary = masha * 1.1;
+        var denisNewSalary = denis * 1.1;
+        var kristinaNewSalary = kristina * 1.1;
+        System.out.println("Маша теперь получает " + mashaNewSalary + " рублей. Годовой доход вырос на " + (mashaNewSalary-masha) + " рублей");
+        System.out.println("Денис теперь получает " + denisNewSalary + " рублей. Годовой доход вырос на " + (denisNewSalary-denis) + " рублей");
+        System.out.println("Кристина теперь получает " + kristinaNewSalary + " рублей. Годовой доход вырос на " + (kristinaNewSalary-kristina) + " рублей");
+
+
+
+        
+
 
         
 
